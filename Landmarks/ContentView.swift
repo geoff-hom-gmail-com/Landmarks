131208f22b9ac09affc1bf47ea577b8574c74a71
Geoff Hom <geoffhom@gmail.com>
2019-08-19 23:42:31 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Geoff Hom on 8/19/19.
+//  Copyright © 2019 Geoff Hom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
