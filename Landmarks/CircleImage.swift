b0680b3ce84e2459366b9a074b7a6cb24cf616b4
Geoff Hom <geoffhom@gmail.com>
2019-08-27 23:12:01 -0700

    Version 1.0 (build 2). Testing transfer to GitHub and cloning to new laptop.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Geoff Hom on 8/20/19.
+//  Copyright © 2019 Geoff Hom. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
